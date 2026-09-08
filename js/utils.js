d51f8c9eb979362ec39be8d1aae9f48ffd2ceed6
saifabad511-art <saifabad511@gmail.com>
2026-09-08 14:51:36 +0300

    تحديث ملفات المشروع - الإصدار v3

---
+++ js/utils.js
@@ -0,0 +1,6 @@
+// Interactive UI Utilities
+window.nexusUtils = {
+  showContactAlert(message) {
+    alert(message || "شكراً لاهتمامك! تم تسجيل طلب الاستشارة بنجاح وسيتواصل معك الفريق الهندسي.");
+  }
+};
